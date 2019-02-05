03930650bdfa185d74d87b615772bce63a9a2f87
Matthew O'Mara <tuh39725@temple.edu>
2019-02-05 18:43:15 -0500

    fixed file read and struct implementation
    read_config.c now uses proper implementation of
    config_reader.h to initialize struct Config,
    const char conf_types, and enum TYPES.
    I was doing it wrong!
    
    note
    in order to store the result of a sscanf operation on
     a regular variable you should precede its identifier
     with the reference operator, i.e. an ampersand sign

--- config_reader.c
+++ config_reader.c
@@ -3,30 +3,31 @@
 Config* load_config() {
 
   Config* this_config = (Config*)malloc(sizeof(Config));
-  this_config->conf_types = {"TIME_A", "TIME_B", "PARAM_A", "PARAM_B", "OTHER"};
-  //
-  // // opens the config file
-  // FILE * fp;
-  // fp = fopen( "config.file", "r" );
-  //
-  // // buffers for config file line reading
-  // char search_str[LINE_LENGTH];
-  // char line[LINE_LENGTH];
-  //
-  // // for line in config file
-  // int i;
-  // while ( fgets( line, LINE_LENGTH, fp ) != NULL ) {
-  //     // for parameter names we are searching for
-  //     sscanf( line, "PROB_QUIT %f\n", this_config->PROB_QUIT );
-  //     for (i=0;i<NUM_PARAMS;i++) {
-  //     strcpy( search_str, this_config->conf_types[i] );
-  //     strcat( search_str, " %d\n" );
-  //     sscanf( line, search_str, this_config->conf_vals[i] );
-  //   }
-  // }
-  //
-  // // closes config file
-  // fclose(fp);
+  const char *conf_types[NUM_PARAMS] = {"TIME_A", "TIME_B", "PARAM_A", "PARAM_B", "OTHER"};
+  TYPES types;
+
+  // opens the config file
+  FILE * fp;
+  fp = fopen( "config.file", "r" );
+
+  // buffers for config file line reading
+  char search_str[LINE_LENGTH];
+  char line[LINE_LENGTH];
+
+  // for line in config file
+  int i;
+  while ( fgets( line, LINE_LENGTH, fp ) != NULL ) {
+      // for parameter names we are searching for
+      sscanf( line, "PROB_QUIT %f\n", &this_config->PROB_QUIT );
+      for (i=0;i<NUM_PARAMS;i++) {
+      strcpy( search_str, conf_types[i] );
+      strcat( search_str, " %d\n" );
+      sscanf( line, search_str, &this_config->conf_vals[i] );
+    }
+  }
+
+  // closes config file
+  fclose(fp);
 
   return this_config;
 }
